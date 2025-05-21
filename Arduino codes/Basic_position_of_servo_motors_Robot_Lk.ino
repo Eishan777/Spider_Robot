7b04006a4281093900698e4bac58841bcdd76344
Eishan777 <eishan1207.be24@chitkarauniversity.edu.in>
2025-05-22 00:40:18 +0530

    Add files via upload

---
+++ Arduino codes/Basic_position_of_servo_motors_Robot_Lk.ino
@@ -0,0 +1,34 @@
+//Robot Lk
+//Robot Lk YouTube Channel - https://www.youtube.com/@RobotLk
+//Robot Lk Website - https://robotlk.com/
+#include <Servo.h>   
+
+Servo servo[4][3];
+
+//define servos' ports
+const int servo_pin[4][3] = { {3, 4, 2}, {6, 7, 5}, {9, 8, 10},{12, 11, 13} };
+
+void setup()
+{
+  //initialize all servos
+  for (int i = 0; i < 4; i++)
+  {
+    for (int j = 0; j < 3; j++)
+    {
+      servo[i][j].attach(servo_pin[i][j]);
+      delay(20);
+    }
+  }
+}
+
+void loop(void)
+{
+  for (int i = 0; i < 4; i++)
+  {
+    for (int j = 0; j < 3; j++)
+    {
+      servo[i][j].write(90);
+      delay(20);
+    }
+  }
+}
